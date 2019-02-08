6781146a04950501017c4865cd966ffd6c79fa39
Matt Murray <matt@observeinc.com>
2019-02-08 10:54:32 -0800

    Export various snowflake driver interval funcs and types This is a squash of 9 commits:
    ==============================================================
    Expose types and stash the raw response from snowflake.
    
    We need this to be able to do fancy, snowflake specific things
    with rows returned from db queries.
    Note that this doesn't solve everything b/c sqlx.Rows is a struct
    instead of an interface and doesn't expose the wrapped interface
    directly, but that's a problem for another time.
    
    Change-Id: I7b8619d6136ecd80500ad246e6844a89b5563332
    
    ==============================================================
    Export setter methods for ExecResponse
    
    Change-Id: Id1a47e262a1d1b73403a302f39e82366500a3c2d
    
    ==============================================================
    Export SnowflakeRestful
    
    Add a magic comment to use the specified request ID
    
    Allows the caller to use that request ID to get updates on the query's
    progress.
    
    Change-Id: Ibd7086facdfd7cd4327e63593e3059ede3a59f02
    
    ==============================================================
    Export SnowflakeRestful
    
    With some reflection, we can use this type to post GET requests (and
    other requests) to Snowflake ourselves.
    
    Change-Id: Ia24d542b84edc948737f05ac421c706e1c2f9a7a
    
    ==============================================================
    Fix bug in requestID parsing and simplify snowflakeRestful export
    
    Fix some issues with the two previous commits:
    1) remove the panic that I put in for testing (duh!). Luckily this panic
       does not break any existing clients.
    2) use export.go to export snowflakeRestful, which is simpler and less
       intrusive
    
    Change-Id: I3a30d41dd50aa15a490aa497b88e9635831524d9
    
    ==============================================================
    4. Merge upstream changes
    
    The main merge conflict resolved was to remove the hack of putting
    REQUESTID in a comment in the SQL statement now that the upstream added
    a way to specify the request ID in the context.
    
    Change-Id: I6623ea1b23a648a4a259755ec073cde7f07dcaca
    
    ==============================================================
    export types/method to support Arrow development
    
    ==============================================================
    export simpleTokenAccessor for testing
    
    snowflakeRestful.Token has been replaced by TokenAccessor. In testing,
    we need to provide a fake TokenAccessor, which is simpleTokenAccessor.
    
    Exporting it means we can reuse the fake in our unit tests.
    
    ==============================================================
    exporting StringToValue function

---
+++ export.go
@@ -0,0 +1,61 @@
+package gosnowflake
+
+// This file just exports types, functions and methods as needed
+
+import (
+	"context"
+	"database/sql/driver"
+	"time"
+)
+
+// ExecResponse exports execResponse
+type ExecResponse = execResponse
+
+// ExecResponseRowType exports execResponseRowType
+type ExecResponseRowType = execResponseRowType
+
+// ExecResponseChunk exports execResponseChunk
+type ExecResponseChunk = execResponseChunk
+
+// RawSnowflakeRows exports the "raw" underlying snowflakeRows
+type RawSnowflakeRows = snowflakeRows
+
+// SnowflakeRestful exports snowflakeRestful
+type SnowflakeRestful = snowflakeRestful
+
+// SnowflakeValue exports snowflakeValue
+type SnowflakeValue = snowflakeValue
+
+// ChunkRowType exports chunkRowType
+type ChunkRowType = chunkRowType
+
+// SimpleTokenAccessor exports simpleTokenAccessor
+type SimpleTokenAccessor = simpleTokenAccessor
+
+// ArrowToValue exports arrowToValue
+var ArrowToValue = arrowToValue
+
+// GetExecResponse returns the ExecResponse
+func (sr *snowflakeRows) GetExecResponse() *ExecResponse {
+	return sr.execResp
+}
+
+// GetExecResponse returns the ExecResponse
+func (sr *snowflakeResult) GetExecResponse() *ExecResponse {
+	return sr.execResp
+}
+
+// Setter method for unit testing
+func (sr *snowflakeRows) SetExecResponse(er *ExecResponse) {
+	sr.execResp = er
+}
+
+// Setter method for unit testing
+func (sr *snowflakeResult) SetExecResponse(er *ExecResponse) {
+	sr.execResp = er
+}
+
+// StringToValue exports stringToValue
+func StringToValue(ctx context.Context, dest *driver.Value, srcColumnMeta execResponseRowType, srcValue *string, loc *time.Location, params map[string]*string) error {
+	return stringToValue(context.Background(), dest, srcColumnMeta, srcValue, loc, params)
+}
