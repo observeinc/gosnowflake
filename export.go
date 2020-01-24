bf58bda332954a5fa303312584d57e006d394421
Benoit Sigoure <tsunanet@gmail.com>
2020-01-24 15:15:27 -0800

    export: expose API to the Snowflake chunk downloader
    Change-Id: I3fc7ea9f48fa8095abaf93306bf5ed4373322a96

--- export.go
+++ export.go
@@ -1,5 +1,10 @@
 package gosnowflake
 
+import (
+	"context"
+	"net/http"
+)
+
 // This file just exports types, functions and methods as needed
 
 // Types
@@ -7,8 +12,10 @@ type ExecResponse = execResponse
 type ExecResponseRowType = execResponseRowType
 type ExecResponseChunk = execResponseChunk
 type SnowflakeRows = snowflakeRows
+type SnowflakeChunkDownloader = snowflakeChunkDownloader
 
 // Methods
+
 func (sr *snowflakeRows) GetExecResponse() *ExecResponse {
 	return sr.execResp
 }
@@ -18,10 +25,41 @@ func (sr *snowflakeResult) GetExecResponse() *ExecResponse {
 }
 
 // Setter methods for unit testing
+
 func (sr *snowflakeRows) SetExecResponse(er *ExecResponse) {
 	sr.execResp = er
 }
 
 func (sr *snowflakeResult) SetExecResponse(er *ExecResponse) {
 	sr.execResp = er
+}
+
+// Helpers
+
+func NewSnowflakeRowsDownloader(ctx context.Context, rowType []ExecResponseRowType, rowSet [][]*string, totalRowCount int64, chunkMetas []ExecResponseChunk, chunkHeaders map[string]string) *SnowflakeRows {
+	sc := &snowflakeConn{ // fake connection just to provide a .rest
+		rest: &snowflakeRestful{
+			RequestTimeout: defaultRequestTimeout,
+			Client:         &http.Client{},
+		},
+	}
+	scd := &snowflakeChunkDownloader{
+		ctx:                ctx,
+		sc:                 sc,
+		CurrentChunk:       rowSet,
+		ChunkMetas:         chunkMetas,
+		Total:              totalRowCount,
+		TotalRowIndex:      int64(-1),
+		CellCount:          len(rowType),
+		FuncDownload:       downloadChunk,
+		FuncDownloadHelper: downloadChunkHelper,
+		FuncGet:            getChunk,
+		ChunkHeader:        chunkHeaders,
+	}
+	scd.start()
+	return &SnowflakeRows{
+		sc:              sc,
+		RowType:         rowType,
+		ChunkDownloader: scd,
+	}
 }
