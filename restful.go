5e75276aa75c0acd83ba86945c26e597a0cec948
Ang Li <angl@observeinc.com>
2020-03-13 01:13:27 +0000

    hack: limit the HTTP response size
    So that we don't blow up the client when receiving a very large query
    result.
    
    Change-Id: Ie963ff65d4203bb3ca93500bce1553b1d843badb

--- restful.go
+++ restful.go
@@ -252,7 +252,8 @@ func postRestfulQueryHelper(
 	if resp.StatusCode == http.StatusOK {
 		logger.WithContext(ctx).Infof("postQuery: resp: %v", resp)
 		var respd execResponse
-		if err = json.NewDecoder(resp.Body).Decode(&respd); err != nil {
+		err = newLimitedJSONDecoder(resp.Body).Decode(&respd)
+		if err != nil {
 			logger.WithContext(ctx).Errorf("failed to decode JSON. err: %v", err)
 			return nil, err
 		}
@@ -291,7 +292,7 @@ func postRestfulQueryHelper(
 				return nil, err
 			}
 			respd = execResponse{} // reset the response
-			err = json.NewDecoder(resp.Body).Decode(&respd)
+			err = newLimitedJSONDecoder(resp.Body).Decode(&respd)
 			resp.Body.Close()
 			if err != nil {
 				logger.WithContext(ctx).Errorf("failed to decode JSON. err: %v", err)
@@ -342,7 +343,8 @@ func closeSession(ctx context.Context, sr *snowflakeRestful, timeout time.Durati
 	defer resp.Body.Close()
 	if resp.StatusCode == http.StatusOK {
 		var respd renewSessionResponse
-		if err = json.NewDecoder(resp.Body).Decode(&respd); err != nil {
+		err = newLimitedJSONDecoder(resp.Body).Decode(&respd)
+		if err != nil {
 			logger.WithContext(ctx).Errorf("failed to decode JSON. err: %v", err)
 			return err
 		}
@@ -401,7 +403,7 @@ func renewRestfulSession(ctx context.Context, sr *snowflakeRestful, timeout time
 	defer resp.Body.Close()
 	if resp.StatusCode == http.StatusOK {
 		var respd renewSessionResponse
-		err = json.NewDecoder(resp.Body).Decode(&respd)
+		err = newLimitedJSONDecoder(resp.Body).Decode(&respd)
 		if err != nil {
 			logger.WithContext(ctx).Errorf("failed to decode JSON. err: %v", err)
 			return err
@@ -473,7 +475,8 @@ func cancelQuery(ctx context.Context, sr *snowflakeRestful, requestID UUID, time
 	defer resp.Body.Close()
 	if resp.StatusCode == http.StatusOK {
 		var respd cancelQueryResponse
-		if err = json.NewDecoder(resp.Body).Decode(&respd); err != nil {
+		err = newLimitedJSONDecoder(resp.Body).Decode(&respd)
+		if err != nil {
 			logger.WithContext(ctx).Errorf("failed to decode JSON. err: %v", err)
 			return err
 		}
