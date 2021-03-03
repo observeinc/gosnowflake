b498d550f98d98d7b9a44888baf70ffbba10e31c
Philipp Unterbrunner <ptu@observeinc.com>
2021-03-02 19:45:19 -0800

    shell script for generating parameters.json to run unit tests
    ./parameters.sh > parameters.json
    
    parameters.sh: Add ability to specify context
    
    Allow the context to be passed in as an argument, in case the hardcoded
    value does not match the user's context that they use for eng.
    
    Change-Id: Id971712ebd6b2adc14d9bc0f3d96ffbd9ea6281f
    Change-Id: I724a65a9d40e2eb8eba3757aaf92ef0ee897caa8

---
+++ parameters.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+context=$1
+if [ -z "$context" ]; then
+    context="eng-eks"
+fi
+
+SECRET="$(kubectl --context=$context -n eng get secret snowflake-sfdrivertest-credentials -o=json | jq -r '.data')"
+
+# Create parameters.json
+(
+	echo -n '{"testconnection":{'
+	for E in SNOWFLAKE_TEST_ACCOUNT SNOWFLAKE_TEST_DATABASE SNOWFLAKE_TEST_WAREHOUSE SNOWFLAKE_TEST_USER SNOWFLAKE_TEST_ROLE SNOWFLAKE_TEST_PASSWORD; do
+		echo -n "$COMMA\"$E\":\""`echo ${SECRET} | jq -r --arg E "$E" '.["'$E'"]' | base64 --decode`"\""
+		COMMA=,
+	done
+	echo -n ', "SNOWFLAKE_TEST_JWT_PRIVATE_KEY": "rsa-2048-private-key.p8"'
+	echo -n ', "SNOWFLAKE_TEST_PRIVATE_KEY": "rsa-2048-private-key.pem"'
+	echo '}}'
+) | jq -r '.' > parameters.json
+
+# Read JWT key
+echo ${SECRET} | jq -r '.["SNOWFLAKE_TEST_JWT_PRIVATE_KEY"]' | base64 --decode > rsa-2048-private-key.p8
+
+# Read TEST key
+echo ${SECRET} | jq -r '.["SNOWFLAKE_TEST_PRIVATE_KEY"]' | base64 --decode > rsa-2048-private-key.pem
