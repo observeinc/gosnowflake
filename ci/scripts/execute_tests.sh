8db6618106566439f3272fa008ce21eca2051517
felix.bowing <felix.bowing@observeinc.com>
2025-11-04 20:55:41 +0000

    Allow to run subset of tests

--- ci/scripts/execute_tests.sh
+++ ci/scripts/execute_tests.sh
@@ -2,6 +2,9 @@
 #
 # Build and Test Golang driver
 #
+# Usage: execute_tests.sh [TEST_NAME]
+#   TEST_NAME: Optional test name pattern to pass to -run flag
+#
 set -e
 set -o pipefail
 CI_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -11,6 +14,12 @@ env | grep SNOWFLAKE | grep -v PASS | grep -v SECRET | sort
 cd $TOPDIR
 go install github.com/jstemmer/go-junit-report/v2@latest
 
+# Parse optional test name argument
+TEST_RUN_FLAG=""
+if [[ -n "$1" ]]; then
+  TEST_RUN_FLAG="-run $1"
+fi
+
 if [[ "$HOME_EMPTY" == "yes" ]] ; then
   export GOCACHE=$HOME/go-build
   export GOMODCACHE=$HOME/go-modules
@@ -18,10 +27,10 @@ if [[ "$HOME_EMPTY" == "yes" ]] ; then
 fi
 if [[ -n "$JENKINS_HOME" ]]; then
   export WORKSPACE=${WORKSPACE:-/mnt/workspace}
-  go test $GO_TEST_PARAMS -timeout 90m -race -v . | /home/user/go/bin/go-junit-report -iocopy -out $WORKSPACE/junit-go.xml
+  go test $GO_TEST_PARAMS $TEST_RUN_FLAG -timeout 90m -race -v . | /home/user/go/bin/go-junit-report -iocopy -out $WORKSPACE/junit-go.xml
 else
   set +e
-  go test $GO_TEST_PARAMS -timeout 90m -race -coverprofile=coverage.txt -covermode=atomic -v . | tee test-output.txt
+  go test $GO_TEST_PARAMS $TEST_RUN_FLAG -timeout 90m -race -coverprofile=coverage.txt -covermode=atomic -v . | tee test-output.txt
   TEST_EXIT_CODE=$?
   cat test-output.txt | go-junit-report > test-report.junit.xml
   exit $TEST_EXIT_CODE
