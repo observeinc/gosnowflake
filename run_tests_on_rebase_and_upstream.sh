c2a2bba4c4e04b815704783e6c24e83a889f14c9
felix.bowing <felix.bowing@observeinc.com>
2025-11-05 08:33:13 +0000

    Add script to run rebase and upstream tests

---
+++ run_tests_on_rebase_and_upstream.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+	echo "Error: No git commit hash of upstream provided"
+	echo "Usage: $0 <commit-hash-of-upstream>"
+	exit 1
+fi
+
+COMMIT_HASH="$1"
+
+go mod vendor
+make test >|gosnowflake-test-rebase.txt 2>&1
+git checkout "${COMMIT_HASH}"
+go mod vendor
+make test >|gosnowflake-test-upstream.txt 2>&1
